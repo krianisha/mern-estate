02db322b963fee9003d546979f22b73716d1680a
Kumari Anisha <kumari.anisha.dnb34@gmail.com>
2025-01-17 20:47:09 +0530

    created pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Profile from './pages/Profile';
 export default function App() {
   return (
     <BrowserRouter>
-
+  
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/sign-in' element={<SignIn />} />
